51d8ac4dcea5a7887300030c689a7450dac7bdbb
Walace Silva <wsilva.emp@gmail.com>
2023-06-14 05:56:40 +0000

    docs: added vehicles docs

--- src/vehicle/dto/create-vehicle.dto.ts
+++ src/vehicle/dto/create-vehicle.dto.ts
@@ -1,18 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateVehicleDto {
+  @ApiProperty()
   @IsNotEmpty({"message" : "O campo marca não pode ser vazio"})
   brand: string;
 
+  @ApiProperty()
   @IsNotEmpty({"message" : "O campo modelo não pode ser vazio"})
   model: string;
 
+  @ApiProperty()
   @IsNotEmpty({"message" : "O campo cor não pode ser vazio"})
   color: string;
 
+  @ApiProperty()
   @IsNotEmpty({"message" : "O campo placa não pode ser vazio"})
   registry: string;
 
+  @ApiProperty()
   @IsNotEmpty({"message" : "O campo type de motos não pode ser vazio"})
   kind_of: number;
 }
--- src/vehicle/vehicle.controller.ts
+++ src/vehicle/vehicle.controller.ts
@@ -4,25 +4,37 @@ import { JwtAuthGuard } from 'src/auth/strategies/jwt-auth.guard';
 import { DuplicateEntryFilter } from 'src/exceptions/duplicate.exception.filter';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('vehicles')
 @Controller('vehicles')
 export class VehicleController {
   constructor(private readonly vehicleService: VehicleService) { }
 
-  @UseGuards(JwtAuthGuard)
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Get all vehicles' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Ok.' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
   index() {
     return this.vehicleService.getAll();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Show a vehicle' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Ok.' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
   show(@Param('id') id: string) {
     return this.vehicleService.getById(+id);
   }
 
   @Post()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Create a vehicle' })
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'Created.' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
   @UseFilters(new DuplicateEntryFilter())
   create(@Body() createVehicleDto: CreateVehicleDto) {
     return this.vehicleService.create(createVehicleDto);
@@ -31,12 +43,18 @@ export class VehicleController {
   @Put(':id')
   @UseGuards(JwtAuthGuard)
   @UseFilters(new DuplicateEntryFilter())
+  @ApiOperation({ summary: 'Update a vehicle' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Ok.' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
   update(@Param('id') id: string, @Body() updateVehicleDto: UpdateVehicleDto) {
     return this.vehicleService.update(+id, updateVehicleDto);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Delete a vehicle' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Ok.' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
   delete(@Param('id') id: string, @Res() res) {
     this.vehicleService.delete(+id);
 
