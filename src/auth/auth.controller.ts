b48c5a6bb4615bcdd642ef4b828f5c3e39b71d74
Walace Silva <wsilva.emp@gmail.com>
2023-06-14 16:06:36 +0000

    feat: added logout route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Request, Response, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './strategies/local-auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from './strategies/jwt-auth.guard';
 
+@ApiBearerAuth()
+@ApiTags('authentication')
 @Controller('auth')
 export class AuthController {
 
   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  @HttpCode(200)
+  @UseGuards(LocalAuthGuard)
+  async login(@Request() request) {
+    return this.authService.login(request.user);
+  }
+
+  @Post('logout')
+  @UseGuards(JwtAuthGuard)
+  async logout(@Response() response) {
+    response.setHeader('Set-Cookie', null)
+    return response.sendStatus(HttpStatus.OK);
   }
 }
