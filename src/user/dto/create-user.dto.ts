7c1eab0fabacdf98b7c2587b485dcbb53dcbb8a0
Walace Silva <wsilva.emp@gmail.com>
2023-06-14 05:52:56 +0000

    docs: added user docs

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty({"message" : "O campo nome não pode ser vazio"})
   full_name: string;
 
+  @ApiProperty()
   @IsNotEmpty({"message" : "O campo email não pode ser vazio"})
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty({"message" : "O campo senha não pode ser vazio"})
   password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,7 +4,10 @@ import { JwtAuthGuard } from 'src/auth/strategies/jwt-auth.guard';
 import { DuplicateEntryFilter } from 'src/exceptions/duplicate.exception.filter';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('users')
 @Controller('users')
 export class UserController {
 
@@ -12,19 +15,26 @@ export class UserController {
 
   @Get()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Ok.' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
   index() {
     return this.userService.getAll();
   }
 
   @Get(':id')
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Show a user' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Ok.' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
   show(@Param('id') id: string) {
     return this.userService.getById(+id);
   }
 
   @Post()
   @UseFilters(DuplicateEntryFilter)
-  @UseFilters(new DuplicateEntryFilter())
+  @ApiOperation({ summary: 'Create a user' })
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'Created.' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
@@ -32,12 +42,18 @@ export class UserController {
   @Put(':id')
   @UseGuards(JwtAuthGuard)
   @UseFilters(new DuplicateEntryFilter())
+  @ApiOperation({ summary: 'Update a user' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Ok.' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
   update(@Request() request, @Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(request.user, +id, updateUserDto);
   }
 
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Delete a user' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Ok.' })
+  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
   delete(@Param('id') id: string, @Res() res) {
     this.userService.delete(+id);
 
