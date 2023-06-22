4eca0d0cab65748ae2419490ebdf5b313295deb8
Walace Silva <wsilva.emp@gmail.com>
2023-06-21 22:23:59 -0300

    feat: added devtool module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,13 @@ import { VehicleModule } from './vehicle/vehicle.module';
 import { UserModule } from './user/user.module';
 import { ParkModule } from './park/park.module';
 import { AuthModule } from './auth/auth.module';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
   imports: [
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
     DatabaseModule,
     AuthModule,
     UserModule,
